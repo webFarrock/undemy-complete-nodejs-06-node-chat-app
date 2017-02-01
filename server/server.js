35a9aae474f490ba4e67499a39904f264714d134
webFarrock <webFarrock@mail.ru>
2017-02-01 11:05:22 +0300

    Green new user and alert others

--- server/server.js
+++ server/server.js
@@ -15,6 +15,19 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('new user connected');
 
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app',
+        createdAt: new Date().getTime(),
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined',
+        createdAt: new Date().getTime(),
+    });
+
+
 
     socket.on('createMessage', (message) => {
         console.log('createMessage, ', message);
@@ -24,6 +37,14 @@ io.on('connection', (socket) => {
             text: message.text,
             createdAt: new Date().getTime(),
         });
+
+/*
+        socket.broadcast.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime(),
+        });
+*/
     });
 
     socket.on('disconnect', (socket) => {
